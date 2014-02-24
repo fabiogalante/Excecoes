8f6e602f8b2fd48525e136ab78b54d7d4ce336f2
fabiogalantemans <fabiogalantemans@gmail.com>
2014-02-24 18:34:42 -0300

    Global

---
+++ ManipulandoExcecoes/Controllers/GlobalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManipulandoExcecoes.Controllers
+{
+    public class GlobalController : Controller
+    {
+        //
+        // GET: /Global/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ ManipulandoExcecoes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManipulandoExcecoes.MvcApplication" Language="C#" %>
